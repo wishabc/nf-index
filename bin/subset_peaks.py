e25dcaa94b53f7e498d62c961d0878fdaa27f06f
wishabc <aswq22013@gmail.com>
2023-02-13 13:18:49 -0800

    removed singletons filtering step in model testing

--- bin/subset_peaks.py
+++ bin/subset_peaks.py
@@ -18,9 +18,9 @@ def main(params, normalized_matrix):
     gini = 2 * (q[None, :] - gini / gini[:, -1:]).mean(axis=1)
     means = np.mean(normalized_matrix, axis=1)
 
-    data_norm = DataNormalize(jobs=1, sample_method='raw', cv_fraction=0.2, delta_fraction=0.001)
+    data_norm = DataNormalize(jobs=1, sample_method='raw')
     sampled_peaks_mask = data_norm.select_peaks_uniform(means, np.ones(means.shape[0], dtype=bool))
-    smoothed_gini_samp = data_norm.run_lowess(gini, means, sampled_peaks_mask, frac=0.2, delta_frac=0.001)
+    smoothed_gini_samp = data_norm.run_lowess(gini, means, sampled_peaks_mask, frac=0.2, delta=0.001)
 
     smoothed_gini_final = get_interpolation_for_gini(means, smoothed_gini_samp, sampled_peaks_mask)
     gini_index = gini - smoothed_gini_final
