1a783842ad592ea8d14621c9c63afef3c5d9ea60
m1kss <boytsovs.av@gmail.com>
2023-03-24 00:47:24 -0700

    weights

--- bin/subset_peaks.py
+++ bin/subset_peaks.py
@@ -12,11 +12,20 @@ def get_interpolation_for_gini(x, lowess_est, sampled):
     return interpolated
 
 
-def main(normalized_matrix, binary_matrix, num_peaks, min_peaks_per_sample, save=None):
-    gini = np.cumsum(np.sort(normalized_matrix, axis=1) - normalized_matrix.min(axis=1)[:, None], axis=1)
-    q = np.linspace(0, 1, normalized_matrix.shape[1])
+def main(normalized_matrix, binary_matrix, num_peaks, min_peaks_per_sample, meta_labels, save=None):
+    unique_labels = np.unique(meta_labels)
+    new_norm_matrix = np.zeros(shape=(normalized_matrix.shape[0], unique_labels.shape),
+                                dtype=normalized_matrix.dtype)
+    new_binary_matrix = np.zeros(shape=(normalized_matrix.shape[0], unique_labels.shape),
+                                dtype=binary_matrix.dtype)
+    for label in unique_labels:
+        new_norm_matrix[:, label] = normalized_matrix[:, meta_labels == label].mean(axis=1)
+        new_binary_matrix[:, label] = binary_matrix[:, meta_labels == label].mean(axis=1) >= 0.5
+
+    gini = np.cumsum(np.sort(new_norm_matrix, axis=1) - new_norm_matrix.min(axis=1)[:, None], axis=1)
+    q = np.linspace(0, 1, new_norm_matrix.shape[1])
     gini = 2 * (q[None, :] - gini / gini[:, -1:]).mean(axis=1)
-    means = np.mean(normalized_matrix, axis=1)
+    means = np.mean(new_norm_matrix, axis=1)
 
     data_norm = DataNormalize(jobs=1, sample_method='raw')
     sampled_peaks_mask = data_norm.select_peaks_uniform(means, np.ones(means.shape[0], dtype=bool))
@@ -27,8 +36,8 @@ def main(normalized_matrix, binary_matrix, num_peaks, min_peaks_per_sample, save
     gini_argsort = np.argsort(gini_index)[::-1]
     top_gini_mask = gini_index > gini_index[gini_argsort[num_peaks]]
     if min_peaks_per_sample is not None and min_peaks_per_sample != 0:
-        for sample_id in range(normalized_matrix.shape[1]):
-            sample_binary_mask = binary_matrix[:, sample_id]
+        for sample_id in range(new_norm_matrix.shape[1]):
+            sample_binary_mask = new_binary_matrix[:, sample_id]
             chosen_peaks_mask = top_gini_mask * sample_binary_mask
             to_add_peaks = min_peaks_per_sample - chosen_peaks_mask.sum()
             if to_add_peaks <= 0:
@@ -44,7 +53,7 @@ def main(normalized_matrix, binary_matrix, num_peaks, min_peaks_per_sample, save
             top_gini_mask[to_add_argsort[to_add_argsort != 0][:to_add_peaks]] = 1
     if save is not None:
         np.save(f'{save}.npy',
-            normalized_matrix[
+            new_norm_matrix[
                 (top_gini_mask), :
             ]
         )
@@ -59,9 +68,11 @@ if __name__ == '__main__':
         normalized_matrix = f['vsd']
         binary_matrix = f['binary']
     prefix = sys.argv[3]
+    meta_labels = sys.argv[4]
     num_peaks = params['num_peaks']
     min_peaks_per_sample = params.get('min_peaks_per_sample')
-    new_mask = main(normalized_matrix, binary_matrix, num_peaks, min_peaks_per_sample, save=prefix)
+    new_mask = main(normalized_matrix, binary_matrix, num_peaks, min_peaks_per_sample,
+                    meta_labels=meta_labels, save=prefix)
     
     np.savetxt(f'{prefix}.mask.txt', new_mask, fmt="%5i")
     
