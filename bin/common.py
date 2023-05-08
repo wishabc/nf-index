eba1ff4b79f62d411b1199fc3615442432e2885b
wishabc <aswq22013@gmail.com>
2023-05-08 12:10:02 -0700

    removed redundant meta

--- bin/common.py
+++ bin/common.py
@@ -1,4 +1,4 @@
 import os
 
 def get_matrix_path(dir_path_mat, method, data_slice_size, n_components, prefix):
-    return os.path.join(dir_path_mat, f'{prefix}.{method}.{data_slice_size}.{n_components}.npy')
+    return os.path.join(dir_path_mat, f'{method}.{data_slice_size}.{n_components}.{prefix}_mat.npy')
