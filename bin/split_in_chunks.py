8223688d5a338f3f7e3ddda6db9681b580f90a5f
wishabc <aswq22013@gmail.com>
2023-08-16 15:29:41 -0700

    added conda

--- bin/split_in_chunks.py
+++ bin/split_in_chunks.py
@@ -1,12 +1,13 @@
 import numpy as np
 import sys
-
+from tqdm import tqdm
 
 if __name__ == '__main__':
-    # TODO: wrap in function
+    # TODO: wrap in a function
     chunk_size = int(sys.argv[1])
     matrix = np.load(sys.argv[2])
-    for start_index in range(0, matrix.shape[0], chunk_size):
+    print(matrix.shape)
+    for start_index in tqdm(list(range(0, matrix.shape[0], chunk_size))):
         end_index = start_index + chunk_size
         np.savetxt(
             f"{sys.argv[3]}.{start_index}",
