a187d474587ef4fd4e4ec21f7a7eb01f94ec5bf4
wishabc <aswq22013@gmail.com>
2023-08-16 22:53:03 -0700

    fixes in the script

--- bin/variance_partition.R
+++ bin/variance_partition.R
@@ -16,7 +16,7 @@ meta <- as.data.frame(meta)
 start_index <- as.integer(args[2])
 count <- as.integer(args[3])
 file_path <- args[4]
-dhs_meta <- read.delim(args[5])
+dhs_meta <- fread(args[5])
 
 count <- min(count, nrow(dhs_meta) - start_index + 1)
 
