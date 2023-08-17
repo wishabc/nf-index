b3d0f686f792e9052496b358b9848ff614456812
wishabc <aswq22013@gmail.com>
2023-08-16 21:28:44 -0700

    fixes in the script

--- bin/variance_partition.R
+++ bin/variance_partition.R
@@ -17,8 +17,7 @@ start_index <- as.integer(args[2])
 count <- as.integer(args[3])
 file_path <- args[4]
 
-dims <- h5read(file_path, 'vst', read.attributes = FALSE)
-num_cols <- ncol(dims)
+num_cols <- h5ls(file_path, dataset_name)$dim[2]
 
 
 data <- h5read(file_path, 
