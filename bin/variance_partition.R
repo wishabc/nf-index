032bccf75109a575c90ebea1276352cf5373c41f
wishabc <aswq22013@gmail.com>
2023-08-16 20:54:12 -0700

    removed /

--- bin/variance_partition.R
+++ bin/variance_partition.R
@@ -16,13 +16,12 @@ meta <- as.data.frame(meta)
 start_index <- as.integer(args[2])
 count <- as.integer(args[3])
 
-data <- t(
-        h5read(args[4], 
+data <- h5read(args[4], 
         'vst', 
         start=c(1, start_index), 
         count=c(nrow(meta), count)
     )
-)
+data <- t(data)
 
 sample_names <- h5read(args[4], 'sample_names')
 
