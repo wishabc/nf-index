1ea96b8aa91188a9d88dbe76d45f75dfed22ac87
wishabc <aswq22013@gmail.com>
2023-08-17 18:34:28 -0700

    safeFitExtractVarPartModel

--- bin/variance_partition.R
+++ bin/variance_partition.R
@@ -46,9 +46,7 @@ processRow <- function(i) {
 
   if (inherits(varPart, "try-error")) {
     warning(paste("Singular fit error encountered in row", i))
-    na_row <- data.table(rep(NA, length(formula) + 1))
-    print(na_row)
-    print(row_data)
+    na_row <- data.table(t(rep(NA, length(formula) + 1)))
     rownames(na_row) <- rownames(row_data)
   }
   return(varPart)
