80c45636c23ac8111d276027adb9f7c71fc5ee98
wishabc <aswq22013@gmail.com>
2023-08-23 16:56:42 -0700

    fixes with format

--- bin/variance_partition.R
+++ bin/variance_partition.R
@@ -36,6 +36,10 @@ row.names(data) <- row.names(dhs_meta)
 
 meta <- meta[match(sample_names, row.names(meta)), ]
 
+non_numeric_indices <- is.na(as.numeric(meta$preseq_est_max))
+
+# Replace them with NA
+meta$preseq_est_max[non_numeric_indices] <- NA
 meta$log_preseq_est_max <- log(meta$preseq_est_max)
 mean_log_value <- mean(meta$log_preseq_est_max[is.finite(meta$log_preseq_est_max) & !is.na(meta$log_preseq_est_max)], na.rm = TRUE)
 
