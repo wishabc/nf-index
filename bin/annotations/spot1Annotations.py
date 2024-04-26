ed8641ca95ce08cd856c44731d7e15b2935bb960
wishabc <aswq22013@gmail.com>
2024-04-25 18:46:23 -0700

    fixed mem req for spot1 annotations

--- bin/annotations/spot1Annotations.py
+++ bin/annotations/spot1Annotations.py
@@ -5,55 +5,48 @@ import pandas as pd
 import numpy as np
 import gzip
 import sys
-
+import gc
 #Load in Binary Matrix and Metadata for subsampled SPOT score
-binary_file = sys.argv[1]
-#with gzip.open(binary_file, 'rb') as f:
-#    b = np.genfromtxt(f, delimiter='\t', dtype=np.uint8)
 
-b = np.load(binary_file)
-b_int  = b.astype(int)
-b_filtered = pd.DataFrame(b_int)
-del b_int
-del b
+b = np.load(sys.argv[1]).astype(int)
 
 mask_file = sys.argv[2]
 mask_df = pd.read_table(mask_file, header=None)
 mask_df.columns = ['mask']
 #b_filtered = b[mask_df['mask'] == 1]
-print(b_filtered.shape)
+print(b.shape)
 
 samples_file = sys.argv[3]
 metadata = pd.read_table(samples_file)
 
 #Make sure the spot1 scores and matrix are matrix compatible
-spot1 = metadata['SPOT1_score'].values.reshape(1, -1)
-values = b_filtered * spot1
+spot1 = metadata['SPOT1_score'].to_numpy()
+b *= spot1[None, :]
 
 #Replace 0's with nan's for computation
-values_nonzero = np.where(values != 0, values, np.nan)
+b = np.where(b != 0, b, np.nan)
 
 #Calculate metrics across rows
-spot1_std = np.nanstd(values_nonzero, axis=1)
-spot1_min = np.nanmin(values_nonzero, axis=1)
-spot1_mean = np.nanmean(values_nonzero, axis=1)
-spot1_median = np.nanmedian(values_nonzero, axis=1)
-spot1_max = np.nanmax(values_nonzero, axis=1)
-spot1_25 = np.nanpercentile(values_nonzero, 25, axis=1)
-spot1_75 = np.nanpercentile(values_nonzero, 75, axis=1)
+spot1_std = np.nanstd(b, axis=1)
+spot1_mean = np.nanmean(b, axis=1)
 
+spot1_q = np.nanpercentile(b, [0, 0.25, 0.5, 0.75, 1], axis=1)
+
+del b
+gc.collect()
 # Combine all metrics in a dataframe
-spot1_df = {
-    'spot1_std': spot1_std,
-    'spot1_min': spot1_min,
-    'spot1_mean': spot1_mean,
-    'spot1_median': spot1_median,
-    'spot1_max': spot1_max,
-    'spot1_Q1': spot1_25,
-    'spot1_Q3': spot1_75
-}
 
-spot1_metrics = pd.DataFrame(spot1_df)
+order = ['spot1_std', 'spot1_min', 'spot1_mean', 'spot1_median', 'spot1_max', 'spot1_Q1', 'spot1_Q3']
+
+spot1_metrics = pd.DataFrame({
+    'spot1_std': spot1_std,
+    'spot1_min': spot1_q[0],
+    'spot1_mean': spot1_mean,
+    'spot1_median': spot1_q[2],
+    'spot1_max': spot1_q[4],
+    'spot1_Q1': spot1_q[1],
+    'spot1_Q3': spot1_q[3]
+})
 
 #Output Results
-spot1_metrics.to_csv("spot1_metrics.tsv", index=False, header=False, sep="\t")
+spot1_metrics[order].to_csv("spot1_metrics.tsv", index=False, header=False, sep="\t")
