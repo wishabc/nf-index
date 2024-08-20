2947c9687125217c71c8dcf89139e8d4d39db0f0
wishabc <aswq22013@gmail.com>
2024-08-20 09:46:08 -0700

    Refactor add_metadata.py to use non-regex replace for column name typo in metadata['W']

--- bin/post_processing/add_metadata.py
+++ bin/post_processing/add_metadata.py
@@ -5,6 +5,6 @@ import sys
 metadata = pd.read_table(sys.argv[1])
 path_to_results = sys.argv[2]
 metadata['W'] = path_to_results + '/' + metadata['prefix'] + '/' + metadata['prefix'] + '.' + metadata['n_components'].astype(str) + '.W.npy'
-metadata['H'] = metadata['W'].str.replace('.W.npy', '.H.npy')
+metadata['H'] = metadata['W'].str.replace('.W.npy', '.H.npy', regex=False)
 
 metadata.to_csv(sys.argv[3], sep='\t', index=False)
