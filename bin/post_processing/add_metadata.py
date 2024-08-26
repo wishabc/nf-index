063350fd5419b3ebf8fc9c360b1f7943c89b2292
wishabc <aswq22013@gmail.com>
2024-08-25 23:06:32 -0700

    Refactor add_metadata.py to fix column name typo in metadata['W']

--- bin/post_processing/add_metadata.py
+++ bin/post_processing/add_metadata.py
@@ -4,7 +4,7 @@ import sys
 
 metadata = pd.read_table(sys.argv[1])
 path_to_results = sys.argv[2]
-metadata['W'] = path_to_results + '/' + metadata['prefix'] + '/' + metadata['prefix'] + '.' + metadata['n_components'].astype(str) + '.W.npy'
+metadata['W'] = path_to_results + '/' + metadata['prefix'] + '.' + metadata['n_components'].astype(str) + '/' + metadata['prefix'] + '.' + metadata['n_components'].astype(str) + '.W.npy'
 metadata['H'] = metadata['W'].str.replace('.W.npy', '.H.npy', regex=False)
 
 metadata.to_csv(sys.argv[3], sep='\t', index=False)
