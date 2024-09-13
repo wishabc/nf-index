917ca32374af64706e421c0834419ca8684b20a8
wishabc <aswq22013@gmail.com>
2024-09-13 10:52:53 -0700

    Refactor convert_to_anndata.py to update mask names and add new variables

--- bin/convert_to_anndata/convert_to_anndata.py
+++ bin/convert_to_anndata/convert_to_anndata.py
@@ -7,9 +7,23 @@ import os
 
 
 def main(rows_meta, cols_meta, matrix, masks):
-    adata = ad.AnnData(X=matrix, obs=cols_meta, var=rows_meta)
+    adata = ad.AnnData(X=matrix, obs=cols_meta[['peaks_file']], var=rows_meta)
     for mask_name, mask in masks.items():
+        if mask_name == 'blacklist_rows':
+            mask_name = 'non_blacklisted_dhs'
+            mask = ~mask
+        elif mask_name == 'filtered.autosomes':
+            mask_name = 'autosomal_dhs'
+        elif mask_name == 'filtered_DHS':
+            mask_name = 'pseudo_reproduced_dhs'
+        else:
+            continue
+
         adata.varm[mask_name] = mask
+    
+    adata.obsm['n_peaks'] = adata.X.sum(axis=0)
+    adata.varm['n_contributing_samples'] = adata.X.sum(axis=1)
+    adata.varm['autosomal_pseudo_reproduced_dhs'] = adata.varm['autosomal_dhs'] & adata.varm['pseudo_reproduced_dhs']
 
     return adata
 
@@ -22,14 +36,12 @@ if __name__ == '__main__':
 
     samples_meta = pd.read_table(sys.argv[4]).set_index('ag_id').loc[samples_order]
 
-    outpath = sys.argv[5]
-
     row_masks = {}
     for row_mask in sys.argv[6:]:
-        row_mask_name = os.path.basename(row_mask).replace('.mask.txt', '')
+        row_mask_name = os.path.basename(row_mask).replace('.mask.txt', '').replace('binary.index.', '')
         mask = np.loadtxt(row_mask, dtype=bool)
         row_masks[row_mask_name] = mask
 
 
     adata_obj = main(annotated_masterlist, samples_meta, matrix, row_masks)
-    adata_obj.write(outpath)
+    adata_obj.write(sys.argv[5])
