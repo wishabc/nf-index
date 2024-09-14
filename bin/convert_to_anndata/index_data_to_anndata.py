0ba453e381c4f280af6f0e287517a8fd69f1d61f
wishabc <aswq22013@gmail.com>
2024-09-14 12:55:38 -0700

    Refactor convert_to_anndata.py to use more descriptive function name for sparse conversion

--- bin/convert_to_anndata/index_data_to_anndata.py
+++ bin/convert_to_anndata/index_data_to_anndata.py
@@ -6,7 +6,7 @@ import scipy.sparse as sp
 import os
 
 
-def convert_to_sparse_if_needed(matrix, threshold=0.7):
+def convert_to_sparse_if_sufficently_sparse(matrix, threshold=0.7):
     """
     Check the sparsity of a matrix and convert to sparse format if the proportion of zero values exceeds the threshold.
 
@@ -70,7 +70,7 @@ def main(rows_meta, cols_meta, matrix, masks):
 if __name__ == '__main__':
     annotated_masterlist = pd.read_table(sys.argv[1]).set_index('dhs_id')
     samples_order = np.loadtxt(sys.argv[2], dtype=str)
-    matrix = convert_to_sparse_if_needed(np.load(sys.argv[3]).T)
+    matrix = convert_to_sparse_if_sufficently_sparse(np.load(sys.argv[3]).T)
 
     samples_meta = pd.read_table(sys.argv[4]).set_index('ag_id').loc[samples_order]
 
