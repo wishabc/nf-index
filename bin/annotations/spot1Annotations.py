d9e0a1e32bb27d162759bcc9c8af51cc5f69034d
wishabc <aswq22013@gmail.com>
2024-11-13 16:37:06 -0800

    Refactor code to update loading of binary matrix and calculate SPOT1 metrics in spot1Annotations.py

--- bin/annotations/spot1Annotations.py
+++ bin/annotations/spot1Annotations.py
@@ -8,57 +8,17 @@ import scipy.sparse as sp
 import gc
 
 
-# TODO: move to genometools
-def convert_to_sparse_if_sufficently_sparse(matrix, threshold=0.7):
-    """
-    Check the sparsity of a matrix and convert to sparse format if the proportion of zero values exceeds the threshold.
-
-    Parameters:
-    -----------
-    matrix: np.ndarray or scipy.sparse.spmatrix
-        Input matrix to check for sparsity.
-    threshold: float, optional (default=0.y)
-        The threshold for sparsity. If the proportion of zero values exceeds this threshold, 
-        the matrix will be converted to sparse format.
-
-    Returns:
-    --------
-    matrix: np.ndarray or scipy.sparse.spmatrix
-        The original matrix or the converted sparse matrix.
-    """
-    # Check if the matrix is already sparse
-    if sp.issparse(matrix):
-        print("Matrix is already sparse.")
-        return matrix
-    
-    # Calculate the proportion of zeros
-    zero_count = np.count_nonzero(matrix == 0)
-    total_elements = matrix.size
-    sparsity = zero_count / total_elements
-
-    print(f"Sparsity: {sparsity:.2f}")
-
-    # Convert to sparse format if sparsity exceeds the threshold
-    if sparsity > threshold:
-        print("Converting to sparse format.")
-        return sp.csr_matrix(matrix)
-    else:
-        print("Matrix is sufficiently dense; keeping the current format.")
-        return matrix
-
-
 #Load in Binary Matrix and Metadata for subsampled SPOT score
 print('Loading Binary matrix')
-b = np.load(sys.argv[1])
+b = np.load(sys.argv[1]).astype(np.float32)
 print('Loaded Binary matrix')
-b = convert_to_sparse_if_sufficently_sparse(b).astype(float)
 samples_order = np.loadtxt(sys.argv[2], dtype=str)
 
 metadata = pd.read_table(sys.argv[3]).set_index('ag_id').loc[samples_order]
 
 #Make sure the spot1 scores and matrix are matrix compatible
 spot1 = metadata['SPOT1_score'].to_numpy()
-b = b.multiply(spot1[None, :])
+b *= spot1[None, :]
 
 #Replace 0's with nan's for computation
 b = np.where(b != 0, b, np.nan)
