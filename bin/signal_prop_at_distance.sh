5120242f21ae309349749142fa3ad5804d365394
wishabc <aswq22013@gmail.com>
2024-12-11 12:06:41 +0700

    Refactor signal_prop_at_distance.sh to simplify output format and update bedmap command options

--- bin/signal_prop_at_distance.sh
+++ bin/signal_prop_at_distance.sh
@@ -15,13 +15,13 @@ if ! [[ "$DISTANCE" =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 
-echo -e "#length\tsignal\tdistance\tsum_cutcounts\tsample_id" > $DFOUT
+echo -e "#length\tsignal\tdistance\tsample_id" > $DFOUT
 
 zgrep -v '^#' "$PEAKS_FILE" \
     | awk -v OFS="\t" '{print $1, $7, $7 + 1}' \
     | bedtools slop -i stdin -g $CHROM_SIZES -b $DISTANCE \
     | bedops --merge - \
-    | bedmap --echo --sum - \
+    | bedmap --delim '\t' --echo --sum - \
         <(zgrep -v '^#' "$CUTCOUNTS_FILE" | awk -v OFS="\t" '{print $1, $2, $3, 0, $4}') \
     | awk -v OFS='\t' \
         -v total_cutcounts=$SUM_CUTCOUNTS \
