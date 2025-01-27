564d74f899a46d43b64eb61c1664e8e3144a9872
wishabc <aswq22013@gmail.com>
2025-01-28 02:58:17 +0700

    Update filter_segments.py to output results to stdout and adjust argument count

--- bin/index_scripts/filter_segments.py
+++ bin/index_scripts/filter_segments.py
@@ -2,6 +2,6 @@ import pandas as pd
 import sys
 
 
-assert len(sys.argv) == 3
+assert len(sys.argv) == 2
 df = pd.read_table(sys.argv[1]).query('success_fit & ~max_bg_reached')
-df[['#chr', 'start', 'end']].to_csv(sys.argv[2], sep='\t', index=False, header=None)
+df[['#chr', 'start', 'end']].to_csv(sys.stdout, sep='\t', index=False, header=None)
