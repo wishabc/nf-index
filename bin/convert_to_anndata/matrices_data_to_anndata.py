15c61888e7930fef73b3b236a6c23322e59ff547
wishabc <aswq22013@gmail.com>
2025-01-30 18:31:43 +0700

    fix

--- bin/convert_to_anndata/matrices_data_to_anndata.py
+++ bin/convert_to_anndata/matrices_data_to_anndata.py
@@ -22,7 +22,7 @@ def main(adata, meta, matrices):
 
 if __name__ == '__main__':
     adata_obj = read_zarr_backed(sys.argv[1])
-    samples_meta = pd.read_table(sys.argv[2]).set_index('ag_id')
+    samples_meta = pd.read_table(sys.argv[2], low_memory=False).set_index('ag_id')
     missing_indices = adata_obj.obs.index.difference(samples_meta.index)
     if len(missing_indices) > 0:
         print(f"Warning: Missing indices: {missing_indices}")
