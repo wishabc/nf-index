1a5a283a224f8d975d4febe5c56eab222a1c1b95
wishabc <aswq22013@gmail.com>
2025-05-17 23:00:09 +0700

    fixed filter segments step

--- bin/index_scripts/filter_segments.py
+++ bin/index_scripts/filter_segments.py
@@ -3,5 +3,5 @@ import sys
 
 
 assert len(sys.argv) == 2
-df = pd.read_table(sys.argv[1]).query('success_fit & ~max_bg_reached')
+df = pd.read_table(sys.argv[1]).query('success_fit & ~max_bg_reached & fit_type == "segment"')
 df[['#chr', 'start', 'end']].to_csv(sys.stdout, sep='\t', index=False, header=None)
