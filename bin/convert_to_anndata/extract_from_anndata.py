400ed9c213237d41b5b2081f00dcac50fb93c04f
wishabc <aswq22013@gmail.com>
2025-09-27 01:53:05 +0300

    fix

--- bin/convert_to_anndata/extract_from_anndata.py
+++ bin/convert_to_anndata/extract_from_anndata.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
             metadata = matrix_metadata
 
     np.savetxt(args.samples_order, metadata.index, fmt='%s')
-    metadata.reset_index().to_csv(args.samples_meta, sep='\t')
+    metadata.reset_index().to_csv(args.samples_meta, sep='\t', index=False)
 
     for name, matrix in matrices.items():
         if sp.issparse(matrix):
