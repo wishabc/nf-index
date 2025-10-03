7ecc256639fd1677ff49f20cc27b70b5ba3822b6
wishabc <aswq22013@gmail.com>
2025-10-03 03:46:42 +0300

    fix

--- bin/extract_bg_params.py
+++ bin/extract_bg_params.py
@@ -13,8 +13,8 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    data = pd.read_table(args.bg_params_file, na_values=".")
     dhs_ids = pd.read_table(args.masterlist_file, names=['#chr', 'start', 'end', 'dhs_id', 'summit'])['dhs_id'].values
-    data = data.set_index('dhs_id').loc[dhs_ids, :]
+    data = pd.read_table(args.bg_params_file, na_values=".")
+    data = data.set_index('dhs_id').loc[dhs_ids, ['bg_r', 'bg_p']]
     data['mean_bg'] = data['bg_r'] * data['bg_p'] / (1 - data['bg_p'])
     np.save(args.mean_background, data['mean_bg'].values.astype(np.float32))
