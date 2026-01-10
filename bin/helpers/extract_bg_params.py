c236e8d13b41cc97e1b73b943df29a632bd5a831
wishabc <aswq22013@gmail.com>
2026-01-10 03:53:11 +0300

    fix

--- bin/helpers/extract_bg_params.py
+++ bin/helpers/extract_bg_params.py
@@ -13,6 +13,6 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     data = pd.read_table(args.bg_params_file, na_values=".")
-    data = data.set_index('dhs_id').loc[['bg_r', 'bg_p']]
+    data = data.set_index('dhs_id')['bg_r', 'bg_p']
     data['mean_bg'] = data['bg_r'] * data['bg_p'] / (1 - data['bg_p'])
     np.save(args.mean_background, data['mean_bg'].values.astype(np.float32))
